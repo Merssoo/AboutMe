07ecb854f5c23e332c9b877488f594d0eda61f06
Emerson Cardoso Da Trindade <emerson.cardoso.trindade@gmail.com>
2023-07-09 16:16:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode(){
+    const html = document.documentElement
+
+   /* if (html.classList.contains('luz')){
+        html.classList.remove('luz');
+    } else{
+        html.classList.add('luz');
+    } */
+
+    html.classList.toggle('luz');
+
+}
